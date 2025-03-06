7b1a5c57d63a3f7b606b220361f60416566597b0
Aldair Martinez <martinezaldair96@gmail.com>
2025-03-06 10:56:50 -0500

    update services and controllers, add exception handling

---
+++ src/test/java/com/mycompany/veterinaria/controllerTest/MascotaControllerTest.java
@@ -0,0 +1,4 @@
+package com.mycompany.veterinaria.controllerTest;
+
+public class MascotaControllerTest {
+}
---
+++ src/test/java/com/mycompany/veterinaria/serviceTest/DuenhoServiceTest.java
@@ -0,0 +1,4 @@
+package com.mycompany.veterinaria.serviceTest;
+
+public class DuenhoServiceTest {
+}
